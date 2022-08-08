3ba672f34a44827a9eb13827f35fd3efa63ffe6f
viktor-shved <viktor-shved@yandex-team.ru>
2022-08-08 14:04:56 +0300

    dsadsad

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def hello_world(body: RequestBodyModel):
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
