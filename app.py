a974814926651e4e2f50400595477d2dbf45f4fe
viktor-shved <viktor-shved@yandex-team.ru>
2022-08-08 19:06:05 +0300

    dsadsad

--- app.py
+++ app.py
@@ -122,3 +122,6 @@ def hello_world():
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
